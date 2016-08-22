83ce87c3dca3d18e172726342f6401cc3595fa2d
xiaokun mu <test20121212@qq.com>
2016-08-22 13:38:38 +0800

    CamelCase function/variabel name add LICENSE README remove file LICENSE

--- app/Http/Controllers/FrontendMember.php
+++ app/Http/Controllers/FrontendMember.php
@@ -1,13 +1,4 @@
 <?php
-// +----------------------------------------------------------------------
-// | Core : ThinkPHP Copyright (c) 2006-2014 All rights reserved.
-// +----------------------------------------------------------------------
-// | APP  : Copyright (c) 2014-ALL http://wumingmxk.xicp.net rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: merry M  <test20121212@qq.com>
-// +----------------------------------------------------------------------
 // Fontend Member Controller 前台成员基础控制器
 
 namespace App\Http\Controllers;

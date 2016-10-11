cf9b8aa4de50b0d2c3fdd1a8da0fd472c60b2e0e
xiaokun mu <test20121212@qq.com>
2016-10-11 12:36:01 +0800

    Minify ok add Minify config in system config

--- app/Http/Controllers/Minify.php
+++ app/Http/Controllers/Minify.php
@@ -5,6 +5,13 @@ class Minify extends Controller
 {
     public function run($type)
     {
+        $content    = '';
+        $lang_files = '';
+        $files      = request('files');
+        if (!$files) {
+            return '';
+        }
+
         switch ($type) {
             case 'css':
                 $Min          = new \App\Library\CssMin();
@@ -13,54 +20,70 @@ class Minify extends Controller
             case 'js':
                 $Min          = new \App\Library\JSMin('');
                 $content_type = 'application/x-javascript;charset=utf-8';
+                //js类型引入语言包
+                $lang_files = request('lang');
+                $lang_arr   = [];
+                foreach (explode(',', $lang_files) as $lang) {
+                    $lang_arr[$lang] = trans($lang);
+                }
+                if ($lang_arr) {
+                    $content .= 'var lang = ' . json_encode($lang_arr) . ';';
+                }
                 break;
             default:
                 return '';
         }
 
-        $files = request('files');
-        if (!$files) {
-            return '';
+        $cache_time          = config('system.minify_cache_time');
+        $resource_cache_name = md5('Minify_' . $type . '|' . $files . '|' . $lang_files);
+        $resource_cache      = cache($resource_cache_name);
+        $current_time        = \Carbon\Carbon::now()->getTimestamp();
+        $last_change_time    = $resource_cache['time'] ? $resource_cache['time'] : $current_time;
+
+        $request          = request();
+        $page_was_updated = ($request->hasHeader('If-Modified-Since') and strtotime($request->header('If-Modified-Since')) == $last_change_time);
+        $etag_match       = ($request->hasHeader('If-None-Match') and $request->header('If-None-Match') == $resource_cache_name);
+
+        //debug remove test cache
+        if (config('app.debug') and $etag_match and !$resource_cache) {
+            $page_was_updated = $etag_match = false;
+        }
+        if (config('app.debug')) {
+
         }
 
-        //TODO debug status
-        //TODO include lang
-
-        $cache_time          = 86400;
-        $resource_cache_name = md5($type . $files);
-        $resource_cache      = cache($resource_cache_name);
-        $last_change_time    = $resource_cache['time'] ? $resource_cache['time'] : \Carbon\Carbon::now()->getTimestamp();
-
-        $request        = request();
-        $PageWasUpdated = ($request->hasHeader('If-Modified-Since') and strtotime($request->header('If-Modified-Since')) == $last_change_time);
-        $DoIDsMatch     = ($request->hasHeader('If-None-Match') and $request->header('If-None-Match') == $resource_cache_name);
-        $PageWasUpdated = $DoIDsMatch = false;
-
-        if ($PageWasUpdated or $DoIDsMatch) {
+        if ($page_was_updated or $etag_match) {
             return response('', 304)
                 ->header('ETag', $resource_cache_name)
                 ->header('Connection', 'close');
         } else {
-            $content = '';
             if ($resource_cache) {
                 $content = $resource_cache['content'];
             } else {
+
                 $filesystem = new \Illuminate\Filesystem\Filesystem();
                 foreach (explode(',', $files) as $file) {
+                    //防止使用父级目录
                     if (false !== strrpos($file, '..')) {
                         continue;
                     }
-                    $file_path     = public_path($type . '/' . $file . '.' . $type);
-                    $cache_name    = 'Minify_' . md5($file_path);
-                    $cache_content = cache($cache_name);
-                    if (!$cache_content) {
-                        $file_content  = $filesystem->get($file_path);
-                        $cache_content = $Min->minify($file_content);
-                        cache([$cache_name => $cache_content], 1 / 60);
+
+                    //压缩文件
+                    $file_path = public_path($type . '/' . $file . '.' . $type);
+                    if (false === strrpos($file, '.min')) {
+                        $cache_name    = md5('Minify_' . $file_path);
+                        $cache_content = cache($cache_name);
+                        if (!$cache_content) {
+                            $file_content  = $filesystem->get($file_path);
+                            $cache_content = $Min->minify($file_content);
+                            cache([$cache_name => $cache_content], $cache_time / 60);
+                        }
+                    } else {
+                        $cache_content = $filesystem->get($file_path);
                     }
                     $content .= $cache_content;
                 }
-                cache([$resource_cache_name => ['content' => $content, 'time' => $last_change_time]], 1 / 60);
+                cache([$resource_cache_name => ['content' => $content, 'time' => $last_change_time]], $cache_time / 60);
 
             }
             return response($content, 200)
@@ -68,7 +91,7 @@ class Minify extends Controller
                 ->header('Content-Length', strlen($content))
                 ->header('ETag', $resource_cache_name)
                 ->header('Cache-Control', ' max-age=' . $cache_time)
-                ->header('Expires', gmdate("D, d M Y H:i:s", time() + 31536000) . " GMT")
+                ->header('Expires', gmdate("D, d M Y H:i:s", time() + $cache_time) . " GMT")
                 ->header('Last-Modified', gmdate('D, d M Y H:i:s', $last_change_time) . ' GMT');
         }
     }

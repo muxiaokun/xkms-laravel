1a76fa38493528f75176aa7190fa10ab51700a01
xiaokun mu <test20121212@qq.com>
2016-10-18 17:36:21 +0800

    fix install_info

--- storage/app/xkms/install_info/00sys_seting.php
+++ storage/app/xkms/install_info/00sys_seting.php
@@ -5,12 +5,12 @@ return [
     'privilege'     => [
         'Admin' => [
             'Index'    => [
-                'websiteSet'   => '网站配置',
-                'systemSet'    => '系统配置',
-                'databaseSet'  => '数据库配置',
-                'edit_my_pass' => '修改密码',
-                'clean_cache'  => '清空缓存',
-                'clean_log'    => '清除日志',
+                'websiteSet'  => '网站配置',
+                'systemSet'   => '系统配置',
+                'databaseSet' => '数据库配置',
+                'editMyPass'  => '修改密码',
+                'cleanCache'  => '清空缓存',
+                'cleanLog'    => '清除日志',
             ],
             'Template' => ['index' => '模板管理', 'add' => '添加模板', 'del' => '删除模板', 'edit' => '添加模板'],
         ],
--- storage/app/xkms/install_info/34recruit.php
+++ storage/app/xkms/install_info/34recruit.php
@@ -5,7 +5,7 @@ return [
     'privilege'     => [
         'Admin' => [
             'Recruit'    => ['index' => '招聘管理', 'add' => '新增招聘', 'del' => '删除招聘', 'edit' => '编辑招聘'],
-            'RecruitLog' => ['index' => '应聘记录', 'add' => '新增招聘', 'del' => '删除招聘', 'edit' => '编辑招聘'],
+            'RecruitLog' => ['index' => '应聘记录', 'del' => '删除招聘'],
         ],
         'Home'  => [
             'Recruit' => ['index' => '招聘列表', 'add' => '应聘职位', 'edit' => '查看招聘'],

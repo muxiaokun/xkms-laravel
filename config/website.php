3bce27e431342ed3851371218f8687d48e2569ed
xiaokun mu <test20121212@qq.com>
2016-11-09 21:33:13 +0800

    remove hylogo remove hy info new xklogo

--- config/website.php
+++ config/website.php
@@ -1,11 +1,11 @@
 <?php
 return array(
-    'site_title'       => '皓雅管理系统',
+    'site_title'       => 'XK管理系统',
     'site_domain'      => 'ms.xjhywh.cn',
-    'site_keywords'    => '皓雅管理系统 HTML, CSS, JS, JavaScript, framework, bootstrap, front-end, frontend, web development,jquery,jquery-ui\'"',
-    'site_description' => '皓雅管理系统 简洁、直观、强悍、移动设备优先的前端开发框架，让web开发更迅速、简单。',
+    'site_keywords'    => 'XK管理系统 HTML, CSS, JS, JavaScript, framework, bootstrap, front-end, frontend, web development,jquery,jquery-ui\'"',
+    'site_description' => 'XK管理系统 简洁、直观、强悍、移动设备优先的前端开发框架，让web开发更迅速、简单。',
     'site_other'       => 'HaoYa Culture Dissemination Co,.LTD',
-    'site_company'     => '皓雅文化传播有限公司',
+    'site_company'     => 'XK管理系统',
     'site_phone'       => '',
     'site_telphone'    => '0991-5819279  5819271',
     'site_addr'        => '乌鲁木齐市长沙路1号美林阁小区1号楼1单元1802室',

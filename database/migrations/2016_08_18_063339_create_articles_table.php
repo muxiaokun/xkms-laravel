74f8101ab3feeac8e50345ba99fa144941e1ab0c
xiaokun mu <test20121212@qq.com>
2017-02-16 13:09:53 +0800

    fix Article database migrations bug

--- database/migrations/2016_08_18_063339_create_articles_table.php
+++ database/migrations/2016_08_18_063339_create_articles_table.php
@@ -20,7 +20,7 @@ class CreateArticlesTable extends Migration
             $table->timestamps();
             $table->softDeletes();
 
-            $table->integer('user_id')->unsigned()->comment('发布人编号');
+            $table->integer('user_id')->unsigned()->default(0)->comment('发布人编号');
             $table->longText('access_group_id')->nullable()->comment('可访问会员组');
             $table->string('title', 128)->comment('标题');
             $table->string('author', 64)->comment('作者');
@@ -33,7 +33,7 @@ class CreateArticlesTable extends Migration
             $table->tinyInteger('is_stick')->comment('是否置顶');
             $table->tinyInteger('if_show')->comment('是否显示');
             $table->integer('is_audit')->unsigned()->comment('审核id 0未审核');
-            $table->integer('hits')->unsigned()->comment('点击量');
+            $table->integer('hits')->unsigned()->default(0)->comment('点击量');
             $table->longText('extend')->nullable()->comment('扩展信息');
             $table->longText('attribute')->nullable()->comment('文章属性');
             $table->longText('album')->nullable()->comment('相册信息');

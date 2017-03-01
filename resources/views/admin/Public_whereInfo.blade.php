c0cb94eee0f431c0a7285a131b5ed48c2e3ffb58
xiaokun mu <test20121212@qq.com>
2017-03-01 17:29:58 +0800

    reconfiguration admin.Public_whereInfo.blade

--- resources/views/admin/Public_whereInfo.blade.php
+++ resources/views/admin/Public_whereInfo.blade.php
@@ -1,32 +1,62 @@
-@if ($where_info)
-    <div class="mb10 text-right">
-        <form class="form-inline" role="form" method="get">
+@if (isset($where_info) && is_array($where_info))
+    <form class="form-inline" role="form" method="get">
+        <div class="col-sm-12 mb10 text-right">
             @foreach ($where_info as $input_name => $data)
-                <div class="form-group mr10">
-                    <label>{{ $data['name'] }}</label>
-                    @if ('time' == $data['type'])
+                @if ('time' == $data['type'])
+                    <div class="form-group mr10">
+                        <label>{{ $data['name'] }}</label>
                         <input type="text" name="{{ $input_name }}_start" class="form-control w100"
                                value="{{ request($input_name.'_start') }}" onClick="$(this).val('')"/>
                         @lang('common.to')
                         <input type="text" name="{{ $input_name }}_end" class="form-control w100"
                                value="{{ request($input_name.'_end') }}" onClick="$(this).val('')"/>
                         @datepicker($input_name,$input_name)
-                    @elseif ('input' == $data['type'])
+                    </div>
+                @elseif ('input' == $data['type'])
+                    <div class="form-group mr10">
+                        <label>{{ $data['name'] }}</label>
                         <input type="text" name="{{ $input_name }}" class="form-control w100"
                                value="{{ request($input_name) }}"
                                onClick="$(this).val('')"/>
-                    @elseif ('select' == $data['type'])
-                        <select type="text" name="{{ $input_name }}" class="form-control w100">
+                    </div>
+                @elseif ('select' == $data['type'])
+                    <div class="form-group mr10">
+                        <label>{{ $data['name'] }}</label>
+                        <select name="{{ $input_name }}" class="form-control w100">
                             <option value="">@lang('common.please')@lang('common.selection')</option>
                             @foreach ($data['value'] as $value => $name)
                                 <option value="{{ $value }}"
                                         @if ($value == request($input_name))selected="selected"@endif >{{ $name }}</option>
                             @endforeach
                         </select>
-                    @endif
-                </div>
+                    </div>
+                @endif
             @endforeach
             <input class="btn btn-default" type="submit" value="@lang('common.select')"/>
-        </form>
-    </div>
+        </div>
+        @foreach ($where_info as $input_name => $data)
+            @if ('multilevel_selection' == $data['type'])
+                <div class="col-sm-12 mb10 text-right">
+                    <div id="multilevel_selection_{{ $input_name }}" class="form-group mr10">
+                        <label>{{ $data['name'] }}</label>
+                    </div>
+                    <script type="text/javascript" src="{{ asset('js/M_multilevel_selection.js') }}"></script>
+                    <script type="text/javascript">
+                        var M_multilevel_selection_obj;
+                        $(function () {
+                            var config = {
+                                'out_obj': $('#multilevel_selection_{{ $input_name }}'),
+                                'submit_type': 'id',
+                                'edit_obj': $('<select class="form-control w100"></select>'),
+                                'post_name': '{{ $input_name }}',
+                                'ajax_url': '{{ $data['ajax_url'] }}'
+                            };
+                            M_multilevel_selection_obj = new M_multilevel_selection(config);
+                        });
+                    </script>
+                    <div class="fr w60">&nbsp;</div>
+                </div>
+            @endif
+        @endforeach
+    </form>
 @endif

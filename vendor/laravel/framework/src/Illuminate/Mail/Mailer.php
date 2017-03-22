3812f1dcbd7396cf76b6198ebd58925eb6aeb667
xiaokun mu <test20121212@qq.com>
2017-03-22 23:37:05 +0800

    update framework v5.4.16

--- vendor/laravel/framework/src/Illuminate/Mail/Mailer.php
+++ vendor/laravel/framework/src/Illuminate/Mail/Mailer.php
@@ -9,6 +9,7 @@ use InvalidArgumentException;
 use Illuminate\Support\HtmlString;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Contracts\Mail\Mailer as MailerContract;
 use Illuminate\Contracts\Queue\Factory as QueueContract;
 use Illuminate\Contracts\Mail\Mailable as MailableContract;
@@ -181,7 +182,7 @@ class Mailer implements MailerContract, MailQueueContract
     public function send($view, array $data = [], $callback = null)
     {
         if ($view instanceof MailableContract) {
-            return $view->send($this);
+            return $this->sendMailable($view);
         }
 
         // First we need to parse the view, which could either be a string or an array
@@ -206,6 +207,18 @@ class Mailer implements MailerContract, MailQueueContract
         }
 
         $this->sendSwiftMessage($message->getSwiftMessage());
+    }
+
+    /**
+     * Send the given mailable.
+     *
+     * @param  MailableContract  $mailable
+     * @return mixed
+     */
+    protected function sendMailable(MailableContract $mailable)
+    {
+        return $mailable instanceof ShouldQueue
+                ? $mailable->queue($this->queue) : $mailable->send($this);
     }
 
     /**
@@ -415,8 +428,8 @@ class Mailer implements MailerContract, MailQueueContract
      */
     protected function sendSwiftMessage($message)
     {
-        if ($this->events) {
-            $this->events->dispatch(new Events\MessageSending($message));
+        if (! $this->shouldSendMessage($message)) {
+            return;
         }
 
         try {
@@ -424,6 +437,23 @@ class Mailer implements MailerContract, MailQueueContract
         } finally {
             $this->forceReconnection();
         }
+    }
+
+    /**
+     * Determines if the message can be sent.
+     *
+     * @param  \Swift_Message  $message
+     * @return bool
+     */
+    protected function shouldSendMessage($message)
+    {
+        if (! $this->events) {
+            return true;
+        }
+
+        return $this->events->until(
+            new Events\MessageSending($message)
+        ) !== false;
     }
 
     /**
